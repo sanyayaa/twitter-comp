53d897d971ef60c64ec76b86103476db76e402d0
Sanya <sanyayaatiwari@gmail.com>
2023-09-02 00:14:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Tweet } from 'react-tweet';
+ 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="App px-12 mx-12 sm:grid-cols-1 grid lg:grid-cols-3 gap-8" data-theme="light" >
+     
+      {/* <Tweet id="1659273134879514624" /> */}
+      <Tweet id="1686824395770298369" />
+      <Tweet id="1687011479307059200" />
+      
+      <Tweet id="1659272970903355392" />
+      <Tweet id="1696475381623308502" />
+      <Tweet id="1674060852083802112" />
+      <Tweet id="1647603740227117059" />
+      <Tweet id="1645157312024420354" />
+      <Tweet id="1665031225197690880" />
+      {/* <Tweet id="1680924487032659969" /> */}
+
     </div>
   );
 }
